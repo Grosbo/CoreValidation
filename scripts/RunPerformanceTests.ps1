e6a57ae9cd3ad8da5f03338e83e0a2ca8b7c2eef
Bartosz Lenar <bartoszlenar@gmail.com>
2018-07-30 08:33:41 +1000

    init performance tests

---
+++ scripts/RunPerformanceTests.ps1
@@ -0,0 +1,37 @@
+$ErrorActionPreference = 'Stop'
+
+$scriptsPath = Convert-Path $PSScriptRoot
+. $scriptsPath\Helpers.ps1
+
+Exec "Building before testing" {
+    & $scriptsPath\Build.ps1
+}
+
+$rootDir = Convert-Path "$($scriptsPath)\..\"
+$testsDir = Convert-Path "$($rootDir)\tests"
+
+$artifactsDir = Convert-Path "$($rootDir)\artifacts"
+
+New-Item -ItemType Directory -Force -Path $artifactsDir
+
+$benchmarkArtifactsDir = "$($artifactsDir)\benchmark"
+
+Exec "Cleaning existing artifacts" {
+    if (Test-Path $benchmarkArtifactsDir) {
+        Remove-Item $benchmarkArtifactsDir -Recurse
+    }
+}
+
+New-Item -ItemType Directory -Force -Path $benchmarkArtifactsDir
+
+
+$testNames = @(
+    "Quickstart",
+    "EndToEnd"
+)
+
+for ($i = 0; $i -lt $testNames.length; $i++) {
+    Exec "Running: $($testNames[$i])" {
+        & dotnet run --project $testsDir\CoreValidation.PerformanceTests\CoreValidation.PerformanceTests.csproj -c Release -- --filter "*$($testNames[$i])" --artifacts=$benchmarkArtifactsDir
+    }
+}
---
+++ tests/CoreValidation.PerformanceTests/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace CoreValidation.PerformanceTests
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+        }
+    }
+}
